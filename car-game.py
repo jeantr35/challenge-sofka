b7eedc78633afe1351b99344105ad9c6920ce39d
jeantr35 <jean.trujillo.35@gmail.com>
2021-08-05 10:03:28 -0500

    Creada la clase pista con sus respectivos carriles, ajustada la clase carro y conductor

--- car-game.py
+++ car-game.py
@@ -1,12 +1,21 @@
 import random
 
+menu_pistas = """
+Bienvenido al menu de pistas
+
+0- Alabama internatonal dragway
+1- Atco Dragway
+2- Atlanta dragway
+
+Por favor seleccione una ingresando el numero
+"""
+
 class Carro:
 
-    def __init__(self, modelo, carril, pista, distancia=0):
+    def __init__(self, modelo, conductor, distancia=0):
         self.modelo = modelo
-        self.carril = carril #Carril de la pista a la cual esta asociado
-        self.pista = pista
         self.distancia = distancia
+        self.conductor = None #Conductor aun no asignado
         
 
     def avanzar(self): #Metodo usado para avanzar aleatoriamente
@@ -17,13 +26,28 @@ class Carro:
 
 class Conductor:
 
-    def __init__(self, carro, nombre):
-        self.carro = carro #Carro asociado al conductor
+    def __init__(self, nombre):
         self.nombre = nombre #Nombre del conductor
 
 
+class Pista:
+
+    def __init__(self, carriles, nombre_pista):
+        self.carriles = carriles #Numero de carriles
+        self.nombre_pista = nombre_pista
+        self.carril = [] #Cada elemento de la lista representa un carril con un carro
+
+    def crear_carriles(self): #Metodo para crear los carriles con sus carros
+        self.carril = [Carro('Mazda 3', i, 'Piston') for i in range(self.carriles)] 
+
+
 if __name__ == "__main__":
-    car1 = Carro('Mazda 3', 0, 'Tokio')
-    conductor1 = Conductor(car1, 'Jean')
-    print(f'Hola soy {conductor1.nombre}, y manejo un {conductor1.carro.modelo}')
-    conductor1.carro.avanzar()
+    jugadores = int(input('Por favor ingrese el numero de jugadores: '))
+    nombre_pista = int(input(menu_pistas))
+    menu_pistas = ['Alabama internatonal dragway', 'Atco Dragway', 'Atlanta dragway']
+    pista1 = Pista(jugadores, menu_pistas[nombre_pista])
+    pista1.crear_carriles()
+
+
+
+    
