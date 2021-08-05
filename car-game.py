717d1b126a76891fc9a7fc16db6d8588d18c7d5e
jeantr35 <jean.trujillo.35@gmail.com>
2021-08-04 21:14:30 -0500

    Creada la clase conductor

--- car-game.py
+++ car-game.py
@@ -2,17 +2,28 @@ import random
 
 class Carro:
 
-    def __init__(self, conductor, distancia=0):
-        self.conductor = conductor
+    def __init__(self, modelo, carril, pista, distancia=0):
+        self.modelo = modelo
+        self.carril = carril #Carril de la pista a la cual esta asociado
+        self.pista = pista
         self.distancia = distancia
+        
 
-    def avanzar(self):
+    def avanzar(self): #Metodo usado para avanzar aleatoriamente
         dado = random.randrange(1,7)
         self.distancia += dado * 100
         print(f'El carro ha recorrido un total de {self.distancia} metros')
 
 
+class Conductor:
+
+    def __init__(self, carro, nombre):
+        self.carro = carro #Carro asociado al conductor
+        self.nombre = nombre #Nombre del conductor
+
+
 if __name__ == "__main__":
-    car1 = Carro('Jean')
-    car1.avanzar()
-    car1.avanzar()
+    car1 = Carro('Mazda 3', 0, 'Tokio')
+    conductor1 = Conductor(car1, 'Jean')
+    print(f'Hola soy {conductor1.nombre}, y manejo un {conductor1.carro.modelo}')
+    conductor1.carro.avanzar()
